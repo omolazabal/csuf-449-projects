b5142bb401a9d68e6b2f97f79f1374b66b9e297d
Oscar Olazabal <oscarolazabal.dev@gmail.com>
2019-10-14 14:44:36 -0700

    delete

--- project1/curlfiles.sh
+++ project1/curlfiles.sh
@@ -2,9 +2,14 @@
 # curl --header "Content-type: application/json" --request PUT --data '{"id": 2, "title": "this is a title2", "album": "this ia an album2", "time_len": 4.58, "url_media_file": "This is a media file2", "url_album_chart": "78"}' 
 # curl --header "Content-type: application/json" --request DELETE --data '{"id": 2, "title": "this is a title2", "album": "this ia an album2", "time_len": 4.58, "url_media_file": "This is a media file2", "url_album_chart": "78"}' 
 # curl --header "Content-type: application/json" --request GET --data '{"id": 2, "title": "}'
+
 curl --verbose \
      --header "Content-type: application/json" \
      --request POST \
      --data @data/sample_track.json \
      http://localhost:5000/tracks
 
+curl --verbose \
+     --header "Content-type: application/json" \
+     --request DELETE \
+     http://localhost:5000/tracks?id=3
