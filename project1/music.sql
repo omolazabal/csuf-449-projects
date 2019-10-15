047923636e90f29d5994be7f8ce1b9b7cdc6c6f5
Kevin <kevin5112@yahoo.com>
2019-10-14 22:31:40 -0700

    fixed weird bug

--- project1/music.sql
+++ project1/music.sql
@@ -28,28 +28,26 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users(
     id INTEGER primary key,
     username VARCHAR NOT NULL , 
-    email VARCHAR NOT NULL, 
     user_pass VARCHAR NOT NULL,
     disp_name VARCHAR NOT NULL ,
+    email VARCHAR NOT NULL,
     url_homepage TEXT NULL,
     UNIQUE(username, disp_name)
 );
 
-DROP TABLE IF EXISTS description;
+DROP TABLE IF EXISTS descriptions;
 
-CREATE TABLE description (
+CREATE TABLE descriptions (
     id INTEGER primary key,
-    user_description TEXT NULL,
- 
-
-
+    user_description TEXT NULL
 );
 
 INSERT INTO tracks(title,album_title,time_len,url_media_file) VALUES("Parinod","Beerpng and Bentleys",2.23,"http://thisisawebsite.com");
 INSERT INTO tracks(title,album_title,time_len,url_media_file) VALUES("Zack and Codine","Beerpng and Bentleys",2.55,"http://thisisawebsite2.com");
 INSERT INTO playlist(title,url_ind_tracks,username) VALUES("Parinoid", "http://thisisawebsite.com","username1");
 INSERT INTO playlist(title,url_ind_tracks,username) VALUES("Zack and Codine", "http://thisisawebsite.com2","username1");
-INSERT INTO users(username,user_pass,disp_name,url_homepage) VALUES("username1","Password","username1","http://thisisaurl.com");
-INSERT INTO description(user_description) VALUES ("THis is a description");
+INSERT INTO users(username,user_pass,disp_name,email,url_homepage) VALUES("username1","Password","username1","myemail@gmail.com","http://thisisaurl.com");
+INSERT INTO descriptions(user_description) VALUES("This is a description");
+
 
 COMMIT;
