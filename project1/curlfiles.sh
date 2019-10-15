590b2d782883f9d3d221ca2e130fbccb5d160b7a
Shivam Shah <shivshah12698@gmail.com>
2019-10-14 20:07:57 -0700

    curlfile is done i think

--- project1/curlfiles.sh
+++ project1/curlfiles.sh
@@ -12,3 +12,89 @@ curl --verbose \
      --header "Content-type: application/json" \
      --request DELETE \
      http://localhost:5000/tracks/3
+
+     curl --verbose \
+     --header "Content-type: application/json" \
+     --request PUT \
+     --data @data/sample_track.json \
+     http://localhost:5000/tracks
+
+curl --verbose \
+     --header "Content-type: application/json" \
+     --request GET \
+     http://localhost:5000/tracks/3
+
+
+
+
+
+curl --verbose \
+     --header "Content-type: application/json" \
+     --request POST \
+     --data @data/sample_user.json \
+     http://localhost:5001/users
+
+curl --verbose \
+     --header "Content-type: application/json" \
+     --request DELETE \
+     http://localhost:5001/users/3
+
+     curl --verbose \
+     --header "Content-type: application/json" \
+     --request PUT \
+     --data @data/sample_user.json \
+     http://localhost:5001/users
+
+curl --verbose \
+     --header "Content-type: application/json" \
+     --request GET \
+     http://localhost:5001/users/3
+
+
+
+
+curl --verbose \
+     --header "Content-type: application/json" \
+     --request POST \
+     --data @data/sample_playlist.json \
+     http://localhost:5002/playlists
+
+curl --verbose \
+     --header "Content-type: application/json" \
+     --request DELETE \
+     http://localhost:5002/playlists/3
+
+     curl --verbose \
+     --header "Content-type: application/json" \
+     --request PUT \
+     --data @data/sample_playlist.json \
+     http://localhost:5002/playlists
+
+curl --verbose \
+     --header "Content-type: application/json" \
+     --request GET \
+     http://localhost:5002/playlists/3
+
+
+
+curl --verbose \
+     --header "Content-type: application/json" \
+     --request POST \
+     --data @data/sample_description.json \
+     http://localhost:5003/descriptions
+
+curl --verbose \
+     --header "Content-type: application/json" \
+     --request DELETE \
+     http://localhost:5003/descriptions/3
+
+     curl --verbose \
+     --header "Content-type: application/json" \
+     --request PUT \
+     --data @data/sample_description.json \
+     http://localhost:5003/descriptions
+
+curl --verbose \
+     --header "Content-type: application/json" \
+     --request GET \
+     http://localhost:5003/descriptions/3
