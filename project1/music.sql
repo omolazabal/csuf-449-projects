67d2b4dc8e33ac3f878340a07260e67bb6d1f112
Oscar Olazabal <oscarolazabal.dev@gmail.com>
2019-10-15 14:41:55 -0700

    add playslists

--- project1/music.sql
+++ project1/music.sql
@@ -1,8 +1,8 @@
 -- $ sqlite3 music.db < sqlite.sql
 
 PRAGMA foreign_keys = OFF;
-
 BEGIN TRANSACTION;
+
 DROP TABLE IF EXISTS tracks;
 CREATE TABLE tracks (
     id INTEGER primary key,
@@ -11,10 +11,9 @@ CREATE TABLE tracks (
     time_len FLOAT NOT NULL,
     url_media_file TEXT NOT NULL,
     url_album_chart TEXT NULL
-
 );
-DROP TABLE IF EXISTS playlist;
 
+DROP TABLE IF EXISTS playlist;
 CREATE TABLE playlist(
     id INTEGER primary key,
     title VARCHAR NOT NULL,
@@ -24,7 +23,6 @@ CREATE TABLE playlist(
 );
 
 DROP TABLE IF EXISTS users;
-
 CREATE TABLE users(
     id INTEGER primary key,
     username VARCHAR NOT NULL , 
@@ -36,7 +34,6 @@ CREATE TABLE users(
 );
 
 DROP TABLE IF EXISTS descriptions;
-
 CREATE TABLE descriptions (
     id INTEGER primary key,
     user_description TEXT NULL
@@ -49,5 +46,5 @@ INSERT INTO playlist(title,url_ind_tracks,username) VALUES("Zack and Codine", "h
 INSERT INTO users(username,user_pass,disp_name,email,url_homepage) VALUES("username1","Password","username1","myemail@gmail.com","http://thisisaurl.com");
 INSERT INTO descriptions(user_description) VALUES("This is a description");
 
-
 COMMIT;
+
---
+++ project1/playlists.py
@@ -0,0 +1,79 @@
+
+import flask_api
+from flask import request, jsonify
+from flask_api import status, exceptions
+import pugsql
+
+app = flask_api.FlaskAPI(__name__)
+app.config.from_envvar('APP_CONFIG')
+
+queries = pugsql.module('queries/')
+queries.connect(app.config['DATABASE_URL'])
+
+
+@app.route('/tracks/<int:id>', methods=['GET', 'DELETE', 'PATCH'])
+def track(id):
+    if request.method == 'GET':
+        return get_track(id)
+    elif request.method == 'DELETE':
+        return delete_track(id)
+    elif request.method == 'PATCH':
+        return update_track(id, request.data)
+
+@app.route('/tracks', methods=['POST'])
+def tracks():
+    return insert_track(request.data)
+
+def get_track(id):
+    # GET
+    track = queries.track_by_id(id=id)
+    if track:
+        return track, status.HTTP_200_OK
+    return { "error" : f"Track with id {id} not found" }, status.HTTP_404_NOT_FOUND
+
+def insert_track(track):
+    # POST
+    response = jsonify()
+    required_fields = ['title', 'album_title', 'time_len', 'url_media_file', 'url_album_chart']
+    if not all([field in track for field in required_fields]):
+        raise exceptions.ParseError()
+    try:
+        track['id'] = queries.create_track(**track)
+        response.headers['location'] = f'/tracks/{track["id"]}'
+        response.status_code = 201
+    except Exception as e:
+        response.status_code = 409
+    return response
+
+def delete_track(id):
+    # DELETE
+    if not id:
+        raise exceptions.ParseError()
+    try:
+        queries.delete_track(id=id)
+        return { 'message': f'Deleted 1 track with id {id}' }, status.HTTP_200_OK
+    except Exception as e:
+        return { 'error': str(e) }, status.HTTP_404_NOT_FOUND
+
+def update_track(id, track):
+    # PATCH
+    fields = ['title', 'album_title', 'time_len', 'url_media_file', 'url_album_chart']
+    for field in track.keys():
+        if field not in fields:
+            return { 'error': f'key {field} does not exist' }, status.HTTP_404_NOT_FOUND
+    updates = []
+    query = 'UPDATE tracks SET'
+    for key, value in track.items():
+        query += f' {key}=?,'
+        updates.append(value)
+    query = query[:-1] + ' WHERE id = ?;'
+    updates.append(id)
+    try:
+        queries._engine.execute(query, updates)
+    except Exception as e:
+        return { 'error': str(e) }, status.HTTP_404_NOT_FOUND
+    return get_track(id)
+
+if __name__ == "__main__":
+    app.run()
+
